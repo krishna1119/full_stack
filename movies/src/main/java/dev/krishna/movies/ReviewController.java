baf180e08664932ba888841bc722e0df3dba0f69
Krishna Srinivas <krishnasrinivas2002@gmail.com>
2023-10-09 05:14:48 +0530

    BACKEND DONE

---
+++ movies/src/main/java/dev/krishna/movies/ReviewController.java
@@ -0,0 +1,22 @@
+package dev.krishna.movies;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping(value = "/api/vi/reviews" )
+
+public class ReviewController {
+    @Autowired
+    private ReviewService reviewService;
+
+    @PostMapping
+    public ResponseEntity<Review> createReview(@RequestBody Map<String, String> payload){
+        return new ResponseEntity<Review>(reviewService.createReview(payload.get("reviewBody"), payload.get("imdbId")), HttpStatus.CREATED);
+
+    }
+}
